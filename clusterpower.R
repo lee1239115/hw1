6b53f5f56d03a9d94e509d984df94dea026ffaa2
lee1239115 <61207179+lee1239115@users.noreply.github.com>
2022-03-02 08:48:46 -0500

    Homework 1 for Min

---
+++ clusterpower.R
@@ -0,0 +1,18 @@
+install.packages(''clusterPower'')
+library(clusterPower)
+.set.seed(.p$power
+[1] 0.50817)
+power.sim.poisson(n.sim = 1000, effect.size = log(.75),
+                      alpha = .05,
+                      n.clusters = 10, n.periods = 2,
+                      cluster.size = 210,
+                      btw.clust.var = .5, at.risk.params = 10,
+                      period.effect = log(.004), period.var = 0,
+                      estimation.function = fixed.effect.cluster.
+                      level)
+p <- power.sim.poisson(n.sim=1000, effect.size=log(.75), alpha=.05,
+                      n.clusters=10, n.periods=2, cluster.size=210,
+                      btw.clust.var=.5, at.risk.params=10,
+                      period.effect=log(.004), period.var=0,
+                      estimation.function=fixed.effect.cluster.level)
+exp(3)
